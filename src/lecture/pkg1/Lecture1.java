4e0e6234455f355f41801280016fc0943c4ad1fc
Biii88 <binht7949@gmail.com>
2024-08-24 21:33:01 +0700

    BaiTap

---
+++ src/lecture/pkg1/Lecture1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lecture.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ACER
+ */
+public class Lecture1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // System.out.println("Nhap du lieu: ");
+        // String name = scanner.next(); // nhan du lieu vao tu ban phim la 1 string
+        // String name = scanner.nextLine(); //nhan du lieu vao tu ban phim la 1 line string
+        // System.out.println("name: " + name);
+        // System.out.println("Enter an int number: ");
+        // int number = scanner.nextInt();
+        System.out.println("number");
+        System.out.println("Enter a double: ");
+        double number = scanner.nextDouble();
+    }
+    
+}
